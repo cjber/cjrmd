9249620cd19a233a7df84173df13716779e2be75
cjber <cjberragan@gmail.com>
2020-09-18 15:06:51 +0100

    change tables

--- R/make_table.R
+++ R/make_table.R
@@ -7,18 +7,21 @@
 #' @param data.frame and various kable options
 #' @return kable table
 #' @export
-make_latex_table <- function(df, cap = "", dig = 2, col_names = NA,
+make_latex_table <- function(df, cap = "", dig = 2,
+                             col_names = NA, row_names = NA,
+                             align = c("l", rep("c", ncol(df) - 1)),
                              table_env = "table", ...) {
     options(knitr.kable.NA = "")
     knitr::kable(df,
         digits = dig,
         caption = cap,
         linesep = "", # remove 5 row spacing
-        align = c("l", rep("c", ncol(df) - 1)),
+        align = align,
         longtable = FALSE, booktabs = TRUE, # latex opts
         format = "latex",
         escape = F, # allow maths chars
         col.names = col_names,
+        row.names = row_names,
         table.env = table_env # change to figure* if over two cols
     ) %>%
         kableExtra::kable_styling(
@@ -36,13 +39,16 @@ make_latex_table <- function(df, cap = "", dig = 2, col_names = NA,
 #' @param data.frame and various kable options
 #' @return kable table
 #' @export
-make_html_table <- function(df, cap = "", dig = 2, col_names = NA, ...) {
+make_html_table <- function(df, caption = "", digits = 2,
+                            col_names = NA, row_names = NA,
+                            align = c("l", rep("c", ncol(df) - 1)), ...) {
     options(knitr.kable.NA = "")
     knitr::kable(df,
-        digits = dig,
-        caption = cap,
+        digits = digits,
+        caption = caption,
         format = "html",
-        align = c("l", rep("c", ncol(df) - 1)),
+        align = align,
+        row.names = row_names,
         col.names = col_names
     ) %>%
         kableExtra::kable_styling(position = "center") %>%
